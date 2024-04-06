22b409ce241bb1dce03fb8e1a87e410df1355f0b
Dennis Kountouris <dennisgk2005@gmail.com>
2024-04-05 22:12:38 -0400

    commit

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main():
     ui = light_ui.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
-    qt_interaction_handler.setup_ui_interactions(ui)
+    qt_interaction_handler.setup_ui_interactions(app, ui)
 
     if os.name == "nt":
         MainWindow.show()
     else:
         MainWindow.showFullScreen()
-        
+
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
--- qt_interaction_handler.py
+++ qt_interaction_handler.py
@@ -1,4 +1,6 @@
-def setup_ui_interactions(ui):
+def setup_ui_interactions(app, ui):
     ui.home_button.clicked.connect(lambda: ui.main_display_stack.setCurrentIndex(0))
     ui.action_button.clicked.connect(lambda: ui.main_display_stack.setCurrentIndex(1))
     ui.settings_button.clicked.connect(lambda: ui.main_display_stack.setCurrentIndex(2))
+
+    ui.exit_app_button.clicked.connect(lambda: app.quit())
